98e25a60727a781a70aa7153237965045507a2cb
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2020-04-29 19:56:36 +0000

    CSV import

--- src/Wetter.jl
+++ src/Wetter.jl
@@ -19,14 +19,42 @@ weather_file_dir = String(r.body)
 
 weather_file_html  = parsehtml(weather_file_dir)
 link_elem = weather_file_html.root[2][3].children
+
+file_list = String[]
+
 for elem = filter(x->isa(x, HTMLElement), link_elem)
     filename = elem.attributes["href"]
     if endswith(filename, "hist.zip")
-        r = RemoteFile(url * filename, file = filename, dir = "data")
-        download(r)
+        push!(file_list, filename)
     end
 end
 
+file_list = file_list[1:10]
+
+# Download Data
+for filename = file_list
+    r = RemoteFile(url * filename, file = filename, dir = "data")
+    download(r)
+end
+
+# Import File as Data Table
+
+using CSV
+using ZipFile
+using DataFrames
+
+file_list = readdir("data")
+
+file_name = "data/" * file_list[1]
+
+z = ZipFile.Reader(file_name)
+txt_file = filter(x->endswith(x.name, ".txt"), z.files)[1]
+
+df = CSV.File(txt_file, delim = ";", skipto = 5, missingstrings = ["-999"]) |> DataFrame!
+
 # Next steps:
 # Import file as data table
-# Export file as parquet
+# Export file as zipped CSV
+
+
+# identify the right file in zip
