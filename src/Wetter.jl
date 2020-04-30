9bcde033253016e879b392f6f1b75557467f802f
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2020-04-30 19:43:02 +0000

    Add date parsing

--- src/Wetter.jl
+++ src/Wetter.jl
@@ -42,6 +42,8 @@ end
 using CSV
 using ZipFile
 using DataFrames
+using TimeZones
+using Dates
 
 file_list = readdir("data")
 
@@ -50,11 +52,35 @@ file_name = "data/" * file_list[1]
 z = ZipFile.Reader(file_name)
 txt_file = filter(x->endswith(x.name, ".txt"), z.files)[1]
 
-df = CSV.File(txt_file, delim = ";", skipto = 5, missingstrings = ["-999"]) |> DataFrame!
+df = CSV.File(txt_file, delim = ";", skipto = 5, missingstrings = ["-999"], types = Dict(2 => String)) |> DataFrame!
 
 # Next steps:
 # Import file as data table
+# Document functions
+# Split up Into functions
+# Subset columns and rename
+# Concat all files
 # Export file as zipped CSV
 
 
 # identify the right file in zip
+date_ex = df[1, :MESS_DATUM]
+
+
+const date_format = Dates.DateFormat("yyyymmddHHMM:00:000")
+
+
+
+
+function todate(num)
+    date = DateTime(String(date_ex), date_format)
+    if year(date) < 2000
+        time_zone = tz"Europe/Berlin"
+    else
+        time_zone = tz"UTC"
+    end
+    return ZonedDateTime(date, time_zone)
+end
+
+
+df[:obs_date_utc] = todate.(df[!, :MESS_DATUM])
