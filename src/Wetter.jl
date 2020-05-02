65bd3450e585362c9b8ebfaab59d039d3b617da1
Jeremiah Lewis <4462211+jlewis91@users.noreply.github.com>
2020-05-02 13:55:31 +0000

    Move more logic into functions

--- src/Wetter.jl
+++ src/Wetter.jl
@@ -13,8 +13,19 @@ using DataFrames
 using TimeZones
 using Dates
 using Gumbo
+using CodecZlib
 
+# Compile date format for faster parsing
 const date_format = Dates.DateFormat("yyyymmddHHMM:00:000")
+
+
+function write_df_to_gzip_csv(df, filename)
+    open(GzipCompressorStream, "table.csv.gz", "w") do stream
+        CSV.write(stream, df_full)
+    end
+end
+
+
 
 # Fetch list of available data files
 function fetch_data_file_list()
@@ -42,16 +53,17 @@ end
 # Given filename, download file
 function download_data_file(file_name)
     url = "https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/air_temperature/historical/"
-    r = RemoteFile(url * file_name, file = file_name, dir = "data")
+    r = RemoteFile(url * file_name, file = file_name, dir = "data/zip")
     download(r)
 end
 
 function covert_csv_to_dataframe(file_name)
-    file_name = "data/" * file_name
+    file_name = "data/zip/" * file_name
     global z = ZipFile.Reader(file_name) # Declare as global variable to avoid bug: https://github.com/fhs/ZipFile.jl/issues/14
     txt_file = filter(x->endswith(x.name, ".txt"), z.files)[1]
 
     df = CSV.File(txt_file, delim = ";", missingstrings = ["-999"], types = Dict(2 => String), normalizenames = true) |> DataFrame!
+
     close(z)
 
 
@@ -80,34 +92,41 @@ function covert_csv_to_dataframe(file_name)
     end
 
     df[!, :obs_date_utc] = todate.(df[!, :MESS_DATUM])
-    rename!(df, Dict(:STATIONS_ID => "station_id", :QN => "qn", :TT_10 => "tt_10", :MESS_DATUM => "raw_date_string"))
+    rename!(df, Dict(:STATIONS_ID => "station_id", :QN => "qn", :TT_10 => "tt_10")) # , :MESS_DATUM => "raw_date_string")) #TODO: Figure out how to convert ambiguous and nonexistent timestamps...
     df = df[!, [:station_id, :qn, :tt_10, :obs_date_utc, :raw_date_string]]
     return df
 end
 
 
-# Download Data
-file_list = fetch_data_file_list()
-file_list = file_list[1:10]
-download_data_file.(file_list)
+function download_and_export_data(station_id, file_list)
+    # remove raw data directory (zip dir...)
+    download_data_file.(file_list)
+    # Import File as Data Table
+    file_list = readdir("data/zip")
+    df_list = covert_csv_to_dataframe.(file_list)
+    df_full = vcat(df_list...)
+
+    # Test that the number of NA vals is less than 0.1%
+    @assert mean(df_full.obs_date_utc .== "NA") < 0.001
+
+    write_df_to_gzip_csv(df_full, "data/csv/station_id" * String(station_id) * ".csv.gz")
+end
 
 
-# Import File as Data Table
-file_list = readdir("data")
-file_list = file_list[1:100]
+function download_and_export_data()
 
-df_list = covert_csv_to_dataframe.(file_list)
-# df_list[3]
-df_full = vcat(df_list...)
+    # Download Data
+    file_list = fetch_data_file_list()
 
-# Test that the number of NA vals is less than 0.1%
-@assert mean(df_full.obs_date_utc .== "NA") < 0.001
+    mkdir("data/csv")
 
-
-# df_list[3]
-# reduce(df_list)
-
-# df[!, [:STATIONS_ID]]
+    # 1. filter file list on station_id...
+    file_list = file_list[1:10]
+    for station_id_obj in file_list
+        file_list_station = filter()# filter file list
+        download_and_export_data(station_id_obj.station_id, station_id_obj.file_list)
+    end
+end
 
 
 # Next steps:
@@ -117,16 +136,3 @@ df_full = vcat(df_list...)
 # Subset columns and rename
 # Concat all files
 # Export file as zipped CSV
-
-
-# for item = df_full[!, :obs_date_utc]
-#     try
-#         Dates.format(item, "yyyy-mm-dd HH:MM")
-#     catch
-#         println(item)
-#     end
-# end
-
-
-# df_full[df_full.obs_date_utc .== nothing, :raw_date_string]
-# df_full
